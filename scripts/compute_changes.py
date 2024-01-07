a41f98adf63b82608d77ba76a47f3762e3ce2669
Tianren Dong <tianrendong.dtr@gmail.com>
2024-01-07 01:32:55 -0800

    made changes data year row day column shape

--- scripts/compute_changes.py
+++ scripts/compute_changes.py
@@ -3,39 +3,33 @@ import pandas as pd
 import yfinance as yf
 from tqdm import tqdm
 
-from utils import years, generate_days_in_year, get_last_trading_day, get_same_day_years_ago, calculate_percent_change
+from utils import years, generate_days_in_year, get_same_day_years_ago, calculate_percent_change
 
 def compute_changes(input_file, output_file, interval=1):
-    data = []
-
-    total_days = 365 * len(years)
-    progress_bar = tqdm(total=total_days, desc="Computing Changes")
-
     prices_df = pd.read_csv(input_file)
 
-    # Collect the percent changes for each day across the years
-    for year in years:
-        for curr_date in generate_days_in_year(year):
+    dfs = []
+    dfs.append(pd.DataFrame({'Year': years}))
+
+    for day in tqdm(generate_days_in_year(2023), desc="Processing days"):
+        data = []
+        for year in years:
+            curr_date = day.replace(year=year)
             base_date = get_same_day_years_ago(curr_date, interval)
             curr_price = prices_df.loc[prices_df['Year'] == year, curr_date.strftime("%m-%d")].values[0]
             base_price = prices_df.loc[prices_df['Year'] == year - interval, base_date.strftime("%m-%d")].values[0]
             percent_change = calculate_percent_change(curr_price, base_price)
-            data.append([curr_date.strftime("%Y-%m-%d"), base_date, curr_price, base_price, percent_change])
-            progress_bar.update(1)
+            data.append(percent_change)
+        dfs.append(pd.DataFrame({day.strftime("%m-%d"): data}))
     
-    progress_bar.close()
-
-    # Write the data to a CSV file
-    with open(output_file, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['Current_Date', 'Base_Date', 'Current_Price', 'Base_Price', 'Percent Change'])
-        writer.writerows(data)
+    result_df = pd.concat(dfs, axis=1)
+    result_df.to_csv(output_file)
 
 def main():
     input_file = 'data/spx_closing_prices.csv'
-    compute_changes(input_file, 'data/spx_1_year.csv', 1)
-    compute_changes(input_file, 'data/spx_2_year.csv', 2)
-    compute_changes(input_file, 'data/spx_5_year.csv', 5)
+    compute_changes(input_file, 'data/spx_1_year_changes.csv', 1)
+    compute_changes(input_file, 'data/spx_2_year_changes.csv', 2)
+    compute_changes(input_file, 'data/spx_5_year_changes.csv', 5)
 
 if __name__ == "__main__":
     main()
--- scripts/generate_plots.py
+++ scripts/generate_plots.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 from bokeh.plotting import figure, output_file, show
 from bokeh.models import HoverTool
 from tqdm import tqdm
@@ -23,32 +24,13 @@ def generate_plots(data_file, plot_title, html_file):
     """
     output_file(html_file)
     p = figure(x_range=dates, title=plot_title, y_axis_label="Percent Change", toolbar_location=None, tools="", width=1200, height=600)
-    
-    total_days = 365 * len(years)
-    progress_bar = tqdm(total=total_days, desc="Reading Data")
 
-    with open(data_file, 'r') as file:
-        reader = csv.reader(file)
-        # Skip the header row
-        next(reader)
-
-        # i is the index into the years list
-        # day keeps track of the number days in a year processed
-        # changes is a list of percent changes for each day in a year
-        i, day, changes = 0, 0, []
-        for row in reader:
-            day += 1
-            
-            if day == 366:
-                p.vbar(x=dates, top=changes, line_width=0.9, fill_alpha=0.1, line_color=None)
-                day = 0
-                changes = []
-                i += 1
-                
-            changes.append(float(row[4]))
-            progress_bar.update(1) 
-
-        progress_bar.close() 
+    prices_df = pd.read_csv(data_file)
+    for year in tqdm(years, desc="Processing years"):
+        changes = []
+        for day in generate_days_in_year(2023):
+            changes.append(prices_df.loc[prices_df['Year'] == year, day.strftime("%m-%d")].values[0])
+        p.vbar(x=dates, top=changes, line_width=0.9, fill_alpha=0.1, line_color=None)
 
     # Show detail value when hovering
     hover = HoverTool()
@@ -64,9 +46,9 @@ def generate_plots(data_file, plot_title, html_file):
     show(p)
 
 def main():
-    generate_plots("data/spx_1_year.csv", "S&P 500 1 Year Same Date Change for 1993-2023", "plots/spx_1_year_change.html")
-    generate_plots("data/spx_2_year.csv", "S&P 500 2 Year Same Date Change for 1993-2023", "plots/spx_2_year_change.html")
-    generate_plots("data/spx_5_year.csv", "S&P 500 5 Year Same Date Change for 1993-2023", "plots/spx_5_year_change.html")
+    generate_plots("data/spx_1_year_changes.csv", "S&P 500 1 Year Same Date Change for 1993-2023", "web/plots/spx_1_year_change.html")
+    generate_plots("data/spx_2_year_changes.csv", "S&P 500 2 Year Same Date Change for 1993-2023", "web/plots/spx_2_year_change.html")
+    generate_plots("data/spx_5_year_changes.csv", "S&P 500 5 Year Same Date Change for 1993-2023", "web/plots/spx_5_year_change.html")
 
 if __name__ == "__main__":
     main()
